b1279dd41ea14e5df8565a467ab6f10b97092197
Adions <123456@163.com>
2023-01-05 20:52:26 +0800

    second commit

--- src/main/java/cn/csy/git/GitHello.java
+++ src/main/java/cn/csy/git/GitHello.java
@@ -3,5 +3,6 @@ package cn.csy.git;
 public class GitHello {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
