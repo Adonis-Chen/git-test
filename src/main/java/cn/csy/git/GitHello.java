8b31608a455549870c94348ae8f28818cc83e6f9
Adions <123456@163.com>
2023-01-05 21:51:56 +0800

    hot-fix commit1

--- src/main/java/cn/csy/git/GitHello.java
+++ src/main/java/cn/csy/git/GitHello.java
@@ -5,5 +5,6 @@ public class GitHello {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
